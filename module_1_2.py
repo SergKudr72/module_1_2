30ba79b79714b933d6de5f5cf8155ea797d778e9
SergKudr72 <savat28@yandex.ru>
2024-08-11 17:44:13 +0500

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+total_DZ = 12
+total_hours = 1.5
+name_course = 'Python'
+time_one_task = total_hours / total_DZ
+print('Курс: ',name_course,', всего задач: ',total_DZ,', затрачено часов: ',total_hours,', среднее время выполнения ',time_one_task,' часа.')
